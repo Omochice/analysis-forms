8a6aaaa49e02fe5e28d514b1e346094a0b7f5ce1
mochi-MizLab <h20ms419@hirosaki-u.ac.jp>
2021-01-13 18:55:39 +0900

    Update: update files for upload excel file

--- app/app.py
+++ app/app.py
@@ -1,37 +1,73 @@
-from flask import Flask, render_template, request, session, url_for, redirect
+from flask import Flask, render_template, request, url_for, redirect, flash
 import json
 from pathlib import Path
-from .models import analysis, analysis_form
+import os
+
+from .models import analysis_form, is_allowed_file
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
-pwd = Path("./") # 
+pwd = Path("./")    #
+
+UPLOAD_DIR = pwd / "uploaded"
+ALLOWED_EXT = {"xlsx", "xlx", "csv"}
+app.config["UPLOAD_FOLDER"] = str(UPLOAD_DIR)
+
+app.secret_key = os.environ["SEC_WORD"]
 
 
 @app.route("/")
 def root():
     return render_template("index.html", excel_files=list(pwd.glob("*.xlsx")))
 
-@app.route("/post", methods=["POST"])
-def post():
-    name = request.form["file"]
-    n_groups = request.form["n_groups"]
-    analysis_form(pwd.resolve()/name, int(n_groups))
-    return redirect(url_for("show_all"))
-    # return render_template("index.html")
+
+# @app.route("/post", methods=["POST"])
+# def post():
+#     name = request.form["file"]
+#     n_groups = request.form["n_groups"]
+#     analysis_form(pwd.resolve() / name, int(n_groups))
+#     return redirect(url_for("show_all"))
+#     # return render_template("index.html")
+
 
 @app.route("/show")
 def show_all():
-    analysised_groups = sorted([p.stem for p in  (pwd/ "app" / "static" /"messages").glob("*.json")])
+    analysised_groups = sorted(
+        [p.stem for p in (pwd / "app" / "static" / "messages").glob("*.json")])
     return render_template("show_all.html", groups=analysised_groups)
 
 
 @app.route("/show/<int:groupname>")
 def show(groupname: int):
-    with open(pwd/"app"/"static"/ "messages" / f"{groupname}.json") as f:
+    with open(pwd / "app" / "static" / "messages" / f"{groupname}.json") as f:
         data = json.load(f)
-    return render_template("analysis.html", name=groupname, imgnames=data["images"], messages=data["messages"])
+    return render_template("analysis.html",
+                           name=groupname,
+                           imgnames=data["images"],
+                           messages=data["messages"])
 
+
+@app.route("/upload", methods=["POST"])
+def upload_file():
+    print(request.files)
+    if "upload_file" not in request.files:
+        flash("No file part")
+        return redirect(url_for("root"))
+    else:
+        file = request.files["upload_file"]
+        print(file)
+        n_groups = int(request.form["n_groups"])
+        if file.filename == "":
+            flash("No selecterd file")
+            return redirect(url_for("root"))
+        elif file and is_allowed_file(file.filename, ALLOWED_EXT):
+            filename = secure_filename(file.filename)
+            print(filename)
+            dst = UPLOAD_DIR / filename
+            file.save(dst)
+            analysis_form(dst, n_groups)
+            return redirect(url_for("show_all"))
 
 
 if __name__ == "__main__":
--- app/models.py
+++ app/models.py
@@ -11,6 +11,7 @@ from collections import Counter
 
 matcher = re.compile(r"([1-9 １-９]+)班_(\w+)")
 
+
 def transpose_counter(scores: Counter) -> Tuple[List[str], List[int]]:
     list_score_and_numbers = reversed(sorted(list(scores.items()), key=lambda x: x[0]))
     score, numbers = [], []
@@ -19,37 +20,54 @@ def transpose_counter(scores: Counter) -> Tuple[List[str], List[int]]:
         numbers.append(n)
     return score, numbers
 
+
 def analysis(df: pd.DataFrame) -> None:
-    img_dir = Path(__file__).parent / "static"/ "img"
+    img_dir = Path(__file__).parent / "static" / "img"
     row_names = df.columns.values
     ext = ".png"
-    for row_name in row_names: # 最後の一個はアドバイスが入っていると仮定する
+    for row_name in row_names:    # 最後の一個はアドバイスが入っていると仮定する
         if row_name == row_names[-1]:
             break
         else:
             match_groups = matcher.match(row_name).groups()
-            group_name, evaluates = match_groups 
+            group_name, evaluates = match_groups
             counter = Counter(df[row_name])
             scores, scored_numbers = transpose_counter(counter)
             plt.clf()
-            plt.pie(scored_numbers, labels=scores, counterclock=False, startangle=90, wedgeprops={"linewidth": 3, "edgecolor": "white"}, autopct="%.1f%%")
+            plt.pie(scored_numbers,
+                    labels=scores,
+                    counterclock=False,
+                    startangle=90,
+                    wedgeprops={
+                        "linewidth": 3,
+                        "edgecolor": "white"
+                    },
+                    autopct="%.1f%%")
             plt.title(evaluates)
-            dst = str(img_dir/row_name) + ext
+            dst = str(img_dir / row_name) + ext
             plt.savefig(dst)
-            
-    with open(img_dir.parent / "messages"/(group_name + ".json"), "w" ) as f:
-        json.dump( {
-            "images": [x + ext for x in row_names[:-1]], 
-            "messages": list(map(lambda x: x.replace("\n", "<br>"), df[row_names[-1]]))
-        } , f, ensure_ascii=False)
+
+    with open(img_dir.parent / "messages" / (group_name + ".json"), "w") as f:
+        json.dump(
+            {
+                "images": [x + ext for x in row_names[:-1]],
+                "messages": list(
+                    map(lambda x: x.replace("\n", "<br>"), df[row_names[-1]]))
+            },
+            f,
+            ensure_ascii=False)
 
 
 def analysis_form(filename: os.PathLike, n_group: int) -> None:
     print(filename)
     df = pd.read_excel(filename)
-    n_index = 5 # 左側の使わない列数
-    n_each = (len(df.columns) - 4 ) // n_group
+    n_index = 5    # 左側の使わない列数
+    n_each = (len(df.columns) - 4) // n_group
     for i in range(n_group):
         start = n_index + i * n_each
-        end = n_index + (i +1) *n_each
+        end = n_index + (i + 1) * n_each
         analysis(df.iloc[:, start:end])
+
+
+def is_allowed_file(filename: str, allowd_ext: set) -> bool:
+    return filename.rsplit(".", 1)[1].lower() in allowd_ext
