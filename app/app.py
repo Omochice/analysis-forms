716d234f6d8427b776afcf34f7fb691a02f0a540
mochi-MizLab <h20ms419@hirosaki-u.ac.jp>
2021-01-13 17:52:21 +0900

    update: update to move simply

--- app/app.py
+++ app/app.py
@@ -1,22 +1,38 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, url_for, redirect
+import json
 from pathlib import Path
-from .models import analysis_form
+from .models import analysis, analysis_form
+
 app = Flask(__name__)
+app.config["DEBUG"] = True
+pwd = Path("./") # 
 
 
 @app.route("/")
 def root():
-    pwd = Path("./")
-    print(pwd.resolve())
     return render_template("index.html", excel_files=list(pwd.glob("*.xlsx")))
 
-@app.route("/index", methods=["POST"])
-def index():
-    name = request.form["name"]
-    print(request.form)
-    # 引数のxlsxファイルを分析する
+@app.route("/post", methods=["POST"])
+def post():
+    name = request.form["file"]
+    n_groups = request.form["n_groups"]
+    analysis_form(pwd.resolve()/name, int(n_groups))
+    return redirect(url_for("show_all"))
+    # return render_template("index.html")
 
-    return render_template("index.html")
+@app.route("/show")
+def show_all():
+    analysised_groups = sorted([p.stem for p in  (pwd/ "app" / "static" /"messages").glob("*.json")])
+    return render_template("show_all.html", groups=analysised_groups)
+
+
+@app.route("/show/<int:groupname>")
+def show(groupname: int):
+    with open(pwd/"app"/"static"/ "messages" / f"{groupname}.json") as f:
+        data = json.load(f)
+    return render_template("analysis.html", name=groupname, imgnames=data["images"], messages=data["messages"])
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- app/models.py
+++ app/models.py
@@ -1,4 +1,6 @@
 from typing import Tuple, List
+import json
+from pathlib import Path
 import pandas as pd
 import matplotlib.pyplot as plt
 import japanize_matplotlib
@@ -9,8 +11,8 @@ from collections import Counter
 
 matcher = re.compile(r"([1-9 １-９]+)班_(\w+)")
 
-def dissaemble_scores(scores: Counter) -> Tuple[List[str], List[int]]:
-    list_score_and_numbers = sorted(list(scores.items()), key=lambda x: x[0])
+def transpose_counter(scores: Counter) -> Tuple[List[str], List[int]]:
+    list_score_and_numbers = reversed(sorted(list(scores.items()), key=lambda x: x[0]))
     score, numbers = [], []
     for s, n in list_score_and_numbers:
         score.append(s)
@@ -18,21 +20,36 @@ def dissaemble_scores(scores: Counter) -> Tuple[List[str], List[int]]:
     return score, numbers
 
 def analysis(df: pd.DataFrame) -> None:
+    img_dir = Path(__file__).parent / "static"/ "img"
     row_names = df.columns.values
+    ext = ".png"
     for row_name in row_names: # 最後の一個はアドバイスが入っていると仮定する
-        if row_name == row_names:
+        if row_name == row_names[-1]:
             break
         else:
             match_groups = matcher.match(row_name).groups()
-            group_name, evaluates = match_groups(0), match_groups(1)
+            group_name, evaluates = match_groups 
             counter = Counter(df[row_name])
-            scores, scored_numbers = dissaemble_scores(counter)
+            scores, scored_numbers = transpose_counter(counter)
             plt.clf()
-            plt.pie(scored_numbers, labels=scores, counterclock=False, startangle=90, widgeprops={"linewidth": 3, "edgecolor": "white"})
+            plt.pie(scored_numbers, labels=scores, counterclock=False, startangle=90, wedgeprops={"linewidth": 3, "edgecolor": "white"}, autopct="%.1f%%")
             plt.title(evaluates)
-            plt.savefig(f"{evaluates}.jpg")
+            dst = str(img_dir/row_name) + ext
+            plt.savefig(dst)
             
+    with open(img_dir.parent / "messages"/(group_name + ".json"), "w" ) as f:
+        json.dump( {
+            "images": [x + ext for x in row_names[:-1]], 
+            "messages": list(map(lambda x: x.replace("\n", "<br>"), df[row_names[-1]]))
+        } , f, ensure_ascii=False)
 
 
 def analysis_form(filename: os.PathLike, n_group: int) -> None:
+    print(filename)
     df = pd.read_excel(filename)
+    n_index = 5 # 左側の使わない列数
+    n_each = (len(df.columns) - 4 ) // n_group
+    for i in range(n_group):
+        start = n_index + i * n_each
+        end = n_index + (i +1) *n_each
+        analysis(df.iloc[:, start:end])
